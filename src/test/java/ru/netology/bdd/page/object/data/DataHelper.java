fe28258699ac58da802a5900b7e91a498c962969
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-01-01 00:27:32 +0300

    Update DataHelper.java

--- src/test/java/ru/netology/bdd/page/object/data/DataHelper.java
+++ src/test/java/ru/netology/bdd/page/object/data/DataHelper.java
@@ -1,45 +1,59 @@
 package ru.netology.bdd.page.object.data;
-
 import lombok.Value;
+import java.util.Random;
 
 public class DataHelper {
     private DataHelper() {
-    }
-
-    @Value
-    public static class AuthInfo {
-        private String login;
-        private String password;
     }
 
     public static AuthInfo getAuthInfo() {
         return new AuthInfo("vasya", "qwerty123");
     }
 
-    @Value
-    public static class VerificationCode {
-        private String code;
+    public static VerificationCode getVerificationCode(AuthInfo authInfo) {
+        return new VerificationCode("12345");
     }
 
-    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
-        return new VerificationCode("12345");
+    public static CardInfo getFirstCardInfo() {
+        return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
+
+    public static CardInfo getSecondCardInfo() {
+        return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    }
+
+    public static int generateValidAmount(int balance) {
+        return new Random().nextInt(balance) + 1;
+    }
+
+    public static int generateInvalidAmount(int balance) {
+        return balance * 2;
+    }
+
+    public static double generateAmountWithKopecks(double balance) {
+        return Math.round((balance / 20000) * 100.0) / 100.0;
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    @Value
+    public static class CardInfo {
+        String cardNumber;
+        String testId;
     }
 
     @Value
     public static class TransferCard {
-        private String fromCardNumber;
-        private String amount;
-    }
-
-    public static TransferCard getTransferFirstCard() {
-        return new TransferCard("5559 0000 0000 0001", "5000");
-    }
-
-    public static TransferCard getTransferSecondCard() {
-        return new TransferCard("5559 0000 0000 0002", "3000");
-    }
-
-    public static TransferCard getTransferFirstCardWithKopecks() {
-        return new TransferCard("5559 0000 0000 0001", "500.75");
+        String fromCardNumber;
+        String amount;
     }
 }
