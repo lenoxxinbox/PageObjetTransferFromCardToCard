d84db6170e13b1bb28622314ac804871a0c40422
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-01-01 00:31:37 +0300

    Update TransferPage.java

--- src/test/java/ru/netology/bdd/page/object/page/TransferPage.java
+++ src/test/java/ru/netology/bdd/page/object/page/TransferPage.java
@@ -1,22 +1,45 @@
 package ru.netology.bdd.page.object.page;
-
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.bdd.page.object.data.DataHelper;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TransferPage {
     private SelenideElement transferButton = $("[data-test-id=action-transfer]");
     private SelenideElement transferCanselButton = $("[data-test-id=action-cancel]");
-
     private SelenideElement amountField = $("[data-test-id=amount] input");
     private SelenideElement fromCardField = $("[data-test-id=from] input");
+    private SelenideElement transferHead = $(byText("Пополнение карты"));
+    private SelenideElement errorMessage = $("[data-test-id=error-notification]");
 
-    public TransferPage transfer(DataHelper.TransferCard transferCard) {
-        amountField.setValue(transferCard.getAmount());
-        fromCardField.setValue(transferCard.getFromCardNumber());
+    public TransferPage() {
+        transferHead.shouldBe(visible);
+    }
+
+    public DashboardPage makeValidTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        makeTransfer(amountToTransfer, cardInfo);
+        return new DashboardPage();
+    }
+
+    public void makeTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountField.setValue(amountToTransfer);
+        fromCardField.setValue(cardInfo.getCardNumber());
         transferButton.click();
-        return new TransferPage();
+    }
+
+    public void findErrorMessage(String expectedText) {
+        errorMessage.shouldHave(text(expectedText), Duration.ofSeconds(15)).shouldBe(visible);
+    }
+
+    public DashboardPage canselTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountField.setValue(amountToTransfer);
+        fromCardField.setValue(cardInfo.getCardNumber());
+        transferCanselButton.click();
+        return new DashboardPage();
     }
 
     public TransferPage emptyForm() {
@@ -24,16 +47,16 @@ public class TransferPage {
         return new TransferPage();
     }
 
-    public TransferPage canselTransfer(DataHelper.TransferCard transferCard) {
-        amountField.setValue(transferCard.getAmount());
-        fromCardField.setValue(transferCard.getFromCardNumber());
-        transferCanselButton.click();
+    public TransferPage makeInvalidTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountField.setValue(amountToTransfer);
+        fromCardField.setValue(cardInfo.getCardNumber());
+        transferButton.click();
         return new TransferPage();
     }
 
-    public TransferPage aboveBalance(DataHelper.TransferCard transferCard, int primaryAmount) {
-        amountField.setValue(String.valueOf(primaryAmount * 2));
-        fromCardField.setValue(transferCard.getFromCardNumber());
+    public TransferPage makeTransferWithKopecks(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountField.setValue(amountToTransfer);
+        fromCardField.setValue(cardInfo.getCardNumber());
         transferButton.click();
         return new TransferPage();
     }
